0f27b22c97ab2bf227ac9be325696e8c5a26f259
sho171 <sh.zxcv00@gmail.com>
2021-09-29 22:33:01 +0900

    first commit

---
+++ lib/normalize_text.rb
@@ -0,0 +1,6 @@
+require "normalize_text/version"
+
+module NormalizeText
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/normalize_text/version.rb
@@ -0,0 +1,3 @@
+module NormalizeText
+  VERSION = "0.1.0"
+end
